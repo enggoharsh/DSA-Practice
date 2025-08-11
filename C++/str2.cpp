7d44d30ca0816d84a3ae7adb25417588a9ac3a66
Harsh Mishra <harshmishrasiswa@gmail.com>
2025-08-11 16:09:40 +0530

    practiced new programs

--- C++/str2.cpp
+++ C++/str2.cpp
@@ -1,11 +1,43 @@
+// #include <iostream>
+// using namespace std;
+// void patternP(int num){
+//     for(int i =0;i<num;i++){
+//     for (int j=0;j<num;j++){
+//         cout<<"*";
+//     }
+//     cout<<"\n";
+// }
+// }
+// int main(){ 
+//     int n;
+//     cout<<"Enter the number of lines: ";
+//     cin>>n;
+// patternP(n);
+// return 0;
+// }
+//----------------------------------------------------------------------------------
+//problem 2
 #include <iostream>
 using namespace std;
-int main(){ 
-for(int i =0;i<4;i++){
-    for (int j=0;j<4;j++){
+void patternP(int num){
+    for(int i =0;i<num;i++){
+    for (int j=0;j<=i;j++){
         cout<<"*";
     }
     cout<<"\n";
 }
+}
+int main(){ 
+    int n,t;
+    cout<<"Enter the number of testcases: ";
+    cin>>t;
+    for(int i=0;i<t;i++){
+        cout<<"Enter the number of rows: ";
+        cin>>n;
+        patternP(n);
+
+    }
+    
+
 return 0;
 }
