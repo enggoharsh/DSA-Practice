c13fcb9a0fd37b7a073f66f8391215019d5d7ebb
Harsh Mishra <harshmishrasiswa@gmail.com>
2025-09-06 14:29:48 +0530

    new programs

---
+++ python/Chapter 10 -PS/classes_objects.py
@@ -0,0 +1,12 @@
+'''
+=====classes and objects=====
+'''
+
+class Employee():          # class is been created
+    name = "Harsh" 
+    entry_no = "23bec027"
+    age = 21
+
+
+harsh = Employee()         #two objects harsh and aryan made out from class
+aryan = Employee()
